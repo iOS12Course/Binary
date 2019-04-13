e408ff7e224acf302df186ded68dc68dcaf5241c
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-13 11:42:02 -0500

    Setup Interface

--- Binary/ViewController.swift
+++ Binary/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueEntrytextField: UITextField!
+    @IBOutlet weak var binaryBtn: UIButton!
+    @IBOutlet weak var decimalBtn: UIButton!
+    
+    let placeholder = NSAttributedString(string: "Enter a value...", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), NSAttributedString.Key.font: UIFont(name: "Menlo", size: 36.0)!])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        valueEntrytextField.attributedPlaceholder = placeholder
+        valueEntrytextField.addTarget(self, action: #selector(textFieldTextDidChanged), for: .editingChanged)
+        disableBtns()
+    }
+    
+    @objc func textFieldTextDidChanged() {
+        if valueEntrytextField.text == "" {
+            disableBtns()
+        } else {
+           enableBtns()
+        }
+    }
+    
+    func disableBtns() {
+        binaryBtn.isEnabled = false
+        binaryBtn.alpha = 0.5
+        decimalBtn.isEnabled = false
+        decimalBtn.alpha = 0.5
+    }
+    
+    func enableBtns() {
+        binaryBtn.isEnabled = true
+        binaryBtn.alpha = 1
+        decimalBtn.isEnabled = true
+        decimalBtn.alpha = 1
     }
 
+    @IBAction func binaryBtnWasPressed(_ sender: Any) {
+       
+    }
+    
+    @IBAction func decimalBtnWasPressed(_ sender: Any) {
+    }
+    
 
 }
 
