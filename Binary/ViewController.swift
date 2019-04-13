1da2b8df71b50c249b7ea19dba9512244b8d8535
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-13 11:18:37 -0500

    Initial Commit

---
+++ Binary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binary
+//
+//  Created by Ricardo Herrera Petit on 4/13/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
