20dc8fb8455953c95eb5e10a0e55635daf8f93fa
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-14 18:41:28 -0500

    BinaryDecimal Class

---
+++ Binary/Model/BinaryDecimal.swift
@@ -0,0 +1,9 @@
+//
+//  BinaryDecimal.swift
+//  Binary
+//
+//  Created by Ricardo Herrera Petit on 4/14/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
